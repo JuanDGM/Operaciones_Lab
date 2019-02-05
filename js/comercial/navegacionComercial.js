d1ada8e0c9548a10447393c65200501266b977b5
JuanDGM <juan84_10@hotmail.com>
2019-02-05 10:58:08 -0500

    ocho

--- js/comercial/navegacionComercial.js
+++ js/comercial/navegacionComercial.js
@@ -3,5 +3,7 @@
 function elaborarCotizacion(){
     
     document.getElementById('elaborarCotizacionId').style.display='block';
+    document.getElementById('subModuloElaborarCotizacion').style.border='1px solid #ddd';
+    document.getElementById('subModuloElaborarCotizacion').style.borderBottom='3px solid #29385B';
     
 }
