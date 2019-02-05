4690173ef7b0d1f998ca3ea88b01912ab24b531e
JuanDGM <juan84_10@hotmail.com>
2019-02-05 06:11:43 -0500

    cuatros

---
+++ js/navegacionMenuPrincipal.js
@@ -0,0 +1,47 @@
+
+
+function menuComercial(){
+    
+    document.getElementById('marcoTitleArticleId').innerHTML = '';
+    document.getElementById('marcoTitleArticleId').innerHTML = 'COMERCIAL';
+    
+    document.getElementById('menuComercial').style.display = 'block';
+    document.getElementById('menuRecoleccion').style.display = 'none';
+    document.getElementById('menuMatricula').style.display = 'none';
+    document.getElementById('menuAsignacionTrabajo').style.display = 'none';
+}
+
+function menuRecoleccion(){
+    
+    document.getElementById('marcoTitleArticleId').innerHTML = '';
+    document.getElementById('marcoTitleArticleId').innerHTML = 'RECOLECCIÓN DE MUESTRAS';
+    
+    document.getElementById('menuComercial').style.display = 'none';
+    document.getElementById('menuRecoleccion').style.display = 'block';
+    document.getElementById('menuMatricula').style.display = 'none';
+    document.getElementById('menuAsignacionTrabajo').style.display = 'none';
+}
+
+function menuMatricula(){
+    
+    document.getElementById('marcoTitleArticleId').innerHTML = '';
+    document.getElementById('marcoTitleArticleId').innerHTML = 'MATRICULA DE MUESTRAS';
+    
+    document.getElementById('menuComercial').style.display = 'none';
+    document.getElementById('menuRecoleccion').style.display = 'none';
+    document.getElementById('menuMatricula').style.display = 'block';
+    document.getElementById('menuAsignacionTrabajo').style.display = 'none';
+}
+
+function menuAsignacionTrabajo(){
+    
+    document.getElementById('marcoTitleArticleId').innerHTML = '';
+    document.getElementById('marcoTitleArticleId').innerHTML = 'ASIGNACIÓN DE TRABAJO';
+    
+    document.getElementById('menuComercial').style.display = 'none';
+    document.getElementById('menuRecoleccion').style.display = 'none';
+    document.getElementById('menuMatricula').style.display = 'none';
+    document.getElementById('menuAsignacionTrabajo').style.display = 'block';
+}
+
+
