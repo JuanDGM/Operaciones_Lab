b0ff62aeb933b1403f0341226979afb78976e408
JuanDGM <juan84_10@hotmail.com>
2019-02-05 15:52:20 -0500

    trece

---
+++ js/comercial/makingQuote_DatosTipoCliente.js
@@ -0,0 +1,15 @@
+
+
+function makingQuote_DatosClienteProspecto(){
+    
+    document.getElementById('marcoInputsInfoClienteProspectoId').style.display='flex';
+    document.getElementById('marcoInputsInfoClienteId').style.display='none';
+    
+}
+
+function makingQuote_DatosCliente(){
+    document.getElementById('marcoInputsInfoClienteProspectoId').style.display='none';
+    document.getElementById('marcoInputsInfoClienteId').style.display='flex';
+    
+}
+
