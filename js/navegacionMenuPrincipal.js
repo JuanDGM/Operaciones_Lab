ec70fb3b2a7056ad6c9d247287ed380383a63b9b
JuanDGM <juan84_10@hotmail.com>
2019-02-05 07:38:42 -0500

    seis

--- js/navegacionMenuPrincipal.js
+++ js/navegacionMenuPrincipal.js
@@ -10,6 +10,9 @@ function menuComercial(){
     document.getElementById('menuMatricula').style.display = 'none';
     document.getElementById('menuAsignacionTrabajo').style.display = 'none';
     document.getElementById('menuRegistrarAnalisis').style.display = 'none';
+    document.getElementById('menuFacturacion').style.display = 'none';
+    document.getElementById('menuInteligenciaNegocio').style.display = 'none';
+    document.getElementById('menuPortalCliente').style.display = 'none';
 }
 
 function menuRecoleccion(){
@@ -22,6 +25,10 @@ function menuRecoleccion(){
     document.getElementById('menuMatricula').style.display = 'none';
     document.getElementById('menuAsignacionTrabajo').style.display = 'none';
     document.getElementById('menuRegistrarAnalisis').style.display = 'none';
+    document.getElementById('menuAprobarEnviarResultados').style.display = 'none';
+    document.getElementById('menuFacturacion').style.display = 'none';
+    document.getElementById('menuInteligenciaNegocio').style.display = 'none';
+    document.getElementById('menuPortalCliente').style.display = 'none';
 }
 
 function menuMatricula(){
@@ -34,6 +41,10 @@ function menuMatricula(){
     document.getElementById('menuMatricula').style.display = 'block';
     document.getElementById('menuAsignacionTrabajo').style.display = 'none';
     document.getElementById('menuRegistrarAnalisis').style.display = 'none';
+    document.getElementById('menuAprobarEnviarResultados').style.display = 'none';
+    document.getElementById('menuFacturacion').style.display = 'none';
+    document.getElementById('menuInteligenciaNegocio').style.display = 'none';
+    document.getElementById('menuPortalCliente').style.display = 'none';
 }
 
 function menuAsignacionTrabajo(){
@@ -46,9 +57,13 @@ function menuAsignacionTrabajo(){
     document.getElementById('menuMatricula').style.display = 'none';
     document.getElementById('menuAsignacionTrabajo').style.display = 'block';
     document.getElementById('menuRegistrarAnalisis').style.display = 'none';
+    document.getElementById('menuAprobarEnviarResultados').style.display = 'none';
+    document.getElementById('menuFacturacion').style.display = 'none';
+    document.getElementById('menuInteligenciaNegocio').style.display = 'none';
+    document.getElementById('menuPortalCliente').style.display = 'none';
 }
 
-function menuAsignacionTrabajo(){
+function menuRegistrarAnalisis(){
     
     document.getElementById('marcoTitleArticleId').innerHTML = '';
     document.getElementById('marcoTitleArticleId').innerHTML = 'REGISTRAR ANALISIS';
@@ -58,6 +73,73 @@ function menuAsignacionTrabajo(){
     document.getElementById('menuMatricula').style.display = 'none';
     document.getElementById('menuAsignacionTrabajo').style.display = 'none';
     document.getElementById('menuRegistrarAnalisis').style.display = 'block';
+    document.getElementById('menuAprobarEnviarResultados').style.display = 'none';
+    document.getElementById('menuFacturacion').style.display = 'none';
+    document.getElementById('menuInteligenciaNegocio').style.display = 'none';
+    document.getElementById('menuPortalCliente').style.display = 'none';
 }
 
+function menuAprobarEnviarInforme(){
+    
+    document.getElementById('marcoTitleArticleId').innerHTML = '';
+    document.getElementById('marcoTitleArticleId').innerHTML = 'APROBAR Y ENVIAR INFORME';
+    
+    document.getElementById('menuComercial').style.display = 'none';
+    document.getElementById('menuRecoleccion').style.display = 'none';
+    document.getElementById('menuMatricula').style.display = 'none';
+    document.getElementById('menuAsignacionTrabajo').style.display = 'none';
+    document.getElementById('menuRegistrarAnalisis').style.display = 'none';
+    document.getElementById('menuAprobarEnviarResultados').style.display = 'block';
+    document.getElementById('menuFacturacion').style.display = 'none';
+    document.getElementById('menuInteligenciaNegocio').style.display = 'none';
+    document.getElementById('menuPortalCliente').style.display = 'none';
+}
+
+function menuFacturacion(){
+    
+    document.getElementById('marcoTitleArticleId').innerHTML = '';
+    document.getElementById('marcoTitleArticleId').innerHTML = 'FACTURACIÓN';
+    
+    document.getElementById('menuComercial').style.display = 'none';
+    document.getElementById('menuRecoleccion').style.display = 'none';
+    document.getElementById('menuMatricula').style.display = 'none';
+    document.getElementById('menuAsignacionTrabajo').style.display = 'none';
+    document.getElementById('menuRegistrarAnalisis').style.display = 'none';
+    document.getElementById('menuAprobarEnviarResultados').style.display = 'none';
+    document.getElementById('menuFacturacion').style.display = 'block';
+    document.getElementById('menuInteligenciaNegocio').style.display = 'none';
+    document.getElementById('menuPortalCliente').style.display = 'none';
+}
+
+function menuInteligenciaNegocio(){
+    
+    document.getElementById('marcoTitleArticleId').innerHTML = '';
+    document.getElementById('marcoTitleArticleId').innerHTML = 'INTELIGENCIA DE NEGOCIO';
+    
+    document.getElementById('menuComercial').style.display = 'none';
+    document.getElementById('menuRecoleccion').style.display = 'none';
+    document.getElementById('menuMatricula').style.display = 'none';
+    document.getElementById('menuAsignacionTrabajo').style.display = 'none';
+    document.getElementById('menuRegistrarAnalisis').style.display = 'none';
+    document.getElementById('menuAprobarEnviarResultados').style.display = 'none';
+    document.getElementById('menuFacturacion').style.display = 'none';
+    document.getElementById('menuInteligenciaNegocio').style.display = 'block';
+    document.getElementById('menuPortalCliente').style.display = 'none';
+}
+
+function menuPortalCliente(){
+    
+    document.getElementById('marcoTitleArticleId').innerHTML = '';
+    document.getElementById('marcoTitleArticleId').innerHTML = 'PORTAL DEL CLIENTE';
+    
+    document.getElementById('menuComercial').style.display = 'none';
+    document.getElementById('menuRecoleccion').style.display = 'none';
+    document.getElementById('menuMatricula').style.display = 'none';
+    document.getElementById('menuAsignacionTrabajo').style.display = 'none';
+    document.getElementById('menuRegistrarAnalisis').style.display = 'none';
+    document.getElementById('menuAprobarEnviarResultados').style.display = 'none';
+    document.getElementById('menuFacturacion').style.display = 'none';
+    document.getElementById('menuInteligenciaNegocio').style.display = 'none';
+    document.getElementById('menuPortalCliente').style.display = 'block';
+}
 
