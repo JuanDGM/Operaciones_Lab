678f69f30a4e69109fecb62723367f216f18dd6b
JuanDGM <juan84_10@hotmail.com>
2019-02-07 03:43:58 -0500

    veinteCuatro

--- js/comercial/navegacionComercial.js
+++ js/comercial/navegacionComercial.js
@@ -2,8 +2,34 @@
 
 function elaborarCotizacion(){
     
+    /* Agregar estilos a funcion elaborarCotizacion() */
     document.getElementById('elaborarCotizacionId').style.display='block';
-    document.getElementById('subModuloElaborarCotizacion').style.border='1px solid #ddd';
-    document.getElementById('subModuloElaborarCotizacion').style.borderBottom='3px solid #29385B';
+    document.getElementById('subModuloElaborarCotizacion').style.borderTop='2px solid #757575';
+    document.getElementById('subModuloElaborarCotizacion').style.borderLeft='2px solid #757575';
+    document.getElementById('subModuloElaborarCotizacion').style.borderBottom='0px solid';
+    document.getElementById('subModuloElaborarCotizacion').style.borderRight='0px solid';
     
+    /* quitar estilos de selección de funcion seguimientoCotizaciones() */
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderTop='0px solid';
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderLeft='0px solid';
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderBottom='2px solid #757575';
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderRight='2px solid #757575';
+}
+
+function seguimientoCotizaciones(){
+    
+    /* quitar estilos de selección de funcion elaborarCotizacion() */
+    document.getElementById('elaborarCotizacionId').style.display='none';
+    document.getElementById('subModuloElaborarCotizacion').style.borderTop='0px solid';
+    document.getElementById('subModuloElaborarCotizacion').style.borderLeft='0px solid';
+    document.getElementById('subModuloElaborarCotizacion').style.borderBottom='2px solid #757575';
+    document.getElementById('subModuloElaborarCotizacion').style.borderRight='2px solid #757575';
+    
+    
+    
+    /* Agregar estilos a funcion seguimientoCotizaciones() */
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderTop='2px solid #757575';
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderLeft='2px solid #757575';
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderBottom='0px solid';
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderRight='0px solid';
 }
