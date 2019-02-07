3184f6745956bb0b0c3a3517304b495da9df6413
JuanDGM <juan84_10@hotmail.com>
2019-02-07 12:34:45 -0500

    veintecuatro_

--- js/comercial/navegacionComercial.js
+++ js/comercial/navegacionComercial.js
@@ -15,6 +15,20 @@ function elaborarCotizacion(){
     document.getElementById('subModuloSeguimientoCotizaciones').style.borderLeft='0px solid';
     document.getElementById('subModuloSeguimientoCotizaciones').style.borderBottom='2px solid #757575';
     document.getElementById('subModuloSeguimientoCotizaciones').style.borderRight='2px solid #757575';
+    
+    /* Quitar estilos a crearPortafolio() (btn submodulo) */
+    document.getElementById('containerCrearPortafolioId').style.display='none';
+    document.getElementById('subModuloCrearPortafolio').style.borderTop='0px solid';
+    document.getElementById('subModuloCrearPortafolio').style.borderLeft='0px solid';
+    document.getElementById('subModuloCrearPortafolio').style.borderBottom='2px solid #757575';
+    document.getElementById('subModuloCrearPortafolio').style.borderRight='2px solid #757575';
+    
+    /* Quitar estilos a crearClientes() */
+    document.getElementById('containerCrearClientesId').style.display='none';
+    document.getElementById('subModuloCrearCliente').style.borderTop='0px solid';
+    document.getElementById('subModuloCrearCliente').style.borderLeft='0px solid';
+    document.getElementById('subModuloCrearCliente').style.borderBottom='2px solid #757575';
+    document.getElementById('subModuloCrearCliente').style.borderRight='2px solid #757575';
 }
 
 function seguimientoCotizaciones(){
@@ -26,12 +40,95 @@ function seguimientoCotizaciones(){
     document.getElementById('subModuloElaborarCotizacion').style.borderBottom='2px solid #757575';
     document.getElementById('subModuloElaborarCotizacion').style.borderRight='2px solid #757575';
     
-    
-    
     /* Agregar estilos a funcion seguimientoCotizaciones() */
     document.getElementById('containerSeguimientoCotizaciones').style.display='block';
     document.getElementById('subModuloSeguimientoCotizaciones').style.borderTop='2px solid #757575';
     document.getElementById('subModuloSeguimientoCotizaciones').style.borderLeft='2px solid #757575';
     document.getElementById('subModuloSeguimientoCotizaciones').style.borderBottom='0px solid';
     document.getElementById('subModuloSeguimientoCotizaciones').style.borderRight='0px solid';
+
+    /* Quitar estilos a crearPortafolio() (btn submodulo) */
+    document.getElementById('containerCrearPortafolioId').style.display='none';
+    document.getElementById('subModuloCrearPortafolio').style.borderTop='0px solid';
+    document.getElementById('subModuloCrearPortafolio').style.borderLeft='0px solid';
+    document.getElementById('subModuloCrearPortafolio').style.borderBottom='2px solid #757575';
+    document.getElementById('subModuloCrearPortafolio').style.borderRight='2px solid #757575';
+
+    /* Quitar estilos a crearClientes() */
+    document.getElementById('containerCrearClientesId').style.display='none';
+    document.getElementById('subModuloCrearCliente').style.borderTop='0px solid';
+    document.getElementById('subModuloCrearCliente').style.borderLeft='0px solid';
+    document.getElementById('subModuloCrearCliente').style.borderBottom='2px solid #757575';
+    document.getElementById('subModuloCrearCliente').style.borderRight='2px solid #757575';
 }
+
+
+function crearPortafolio(){
+    
+    /* quitar estilos de selección de funcion elaborarCotizacion() */
+    document.getElementById('elaborarCotizacionId').style.display='none';
+    document.getElementById('subModuloElaborarCotizacion').style.borderTop='0px solid';
+    document.getElementById('subModuloElaborarCotizacion').style.borderLeft='0px solid';
+    document.getElementById('subModuloElaborarCotizacion').style.borderBottom='2px solid #757575';
+    document.getElementById('subModuloElaborarCotizacion').style.borderRight='2px solid #757575';
+    
+    
+    /* quitar estilos a funcion seguimientoCotizaciones() */
+    document.getElementById('containerSeguimientoCotizaciones').style.display='none';
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderTop='0px solid';
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderLeft='0px solid';
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderBottom='2px solid #757575';
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderRight='2px solid #757575';
+    
+    /* Agregar estilos a crearPortafolio() (btn submodulo) */
+    document.getElementById('containerCrearPortafolioId').style.display='block';
+    document.getElementById('subModuloCrearPortafolio').style.borderTop='2px solid #757575';
+    document.getElementById('subModuloCrearPortafolio').style.borderLeft='2px solid #757575';
+    document.getElementById('subModuloCrearPortafolio').style.borderBottom='0px solid';
+    document.getElementById('subModuloCrearPortafolio').style.borderRight='0px solid';
+    
+    
+    /* Quitar estilos a crearClientes() */
+    document.getElementById('containerCrearClientesId').style.display='none';
+    document.getElementById('subModuloCrearCliente').style.borderTop='0px solid';
+    document.getElementById('subModuloCrearCliente').style.borderLeft='0px solid';
+    document.getElementById('subModuloCrearCliente').style.borderBottom='2px solid #757575';
+    document.getElementById('subModuloCrearCliente').style.borderRight='2px solid #757575';
+    
+    
+}
+
+
+function crearClientes(){
+    /* quitar estilos de selección de funcion elaborarCotizacion() */
+    document.getElementById('elaborarCotizacionId').style.display='none';
+    document.getElementById('subModuloElaborarCotizacion').style.borderTop='0px solid';
+    document.getElementById('subModuloElaborarCotizacion').style.borderLeft='0px solid';
+    document.getElementById('subModuloElaborarCotizacion').style.borderBottom='2px solid #757575';
+    document.getElementById('subModuloElaborarCotizacion').style.borderRight='2px solid #757575';
+    
+    
+    /* quitar estilos a funcion seguimientoCotizaciones() */
+    document.getElementById('containerSeguimientoCotizaciones').style.display='none';
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderTop='0px solid';
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderLeft='0px solid';
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderBottom='2px solid #757575';
+    document.getElementById('subModuloSeguimientoCotizaciones').style.borderRight='2px solid #757575';
+    
+    /* Quitar estilos a crearPortafolio() (btn submodulo) */
+    document.getElementById('containerCrearPortafolioId').style.display='none';
+    document.getElementById('subModuloCrearPortafolio').style.borderTop='0px solid';
+    document.getElementById('subModuloCrearPortafolio').style.borderLeft='0px solid';
+    document.getElementById('subModuloCrearPortafolio').style.borderBottom='2px solid #757575';
+    document.getElementById('subModuloCrearPortafolio').style.borderRight='2px solid #757575';
+    
+    
+    /* Agregar estilos a crearClientes() */
+    document.getElementById('containerCrearClientesId').style.display='block';
+    document.getElementById('subModuloCrearCliente').style.borderTop='2px solid #757575';
+    document.getElementById('subModuloCrearCliente').style.borderLeft='2px solid #757575';
+    document.getElementById('subModuloCrearCliente').style.borderBottom='0px solid';
+    document.getElementById('subModuloCrearCliente').style.borderRight='0px solid';
+    
+}
+
