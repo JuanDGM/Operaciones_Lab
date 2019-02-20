00e307db7a4d94bab7242089c2cd2f9a9f2ea1f7
JuanDGM <juan84_10@hotmail.com>
2019-02-20 08:40:55 -0500

    TreintaNuevee

--- js/Recoleccion/desplegarSolicitudRecoleccion.js
+++ js/Recoleccion/desplegarSolicitudRecoleccion.js
@@ -1,25 +1,32 @@
 
 
 function desplegarSolicitudRecoleccion(i){
-    
     $(document).ready(function(){
         var celda1 = "#DireccionOcultar"+i;
         var celda2 = "#TelefonoOcultar"+i;
         var celda3 = "#ContactoOcultar"+i;
+    
+        var fecha = "#FechaId"+i; 
+        var planta = "#plantaId"+i;
         
         var esVisible = $(celda1).is(":visible");
-    if(esVisible){
+    
+        if(esVisible){
         
         $(celda1).hide();
         $(celda2).hide();
         $(celda3).hide();
+        $(fecha).css("border-bottom","solid 0px #ddd");
+        $(planta).css("border-bottom","solid 0px #ddd");
         
-    }else{
+        }else{
+            
         $(celda1).show();
         $(celda2).show();
         $(celda3).show();
-    }
+        
+        $(fecha).css("border-bottom","solid 1px #ddd");
+        $(planta).css("border-bottom","solid 1px #ddd");
+        }
     });
-    
-    
 }
