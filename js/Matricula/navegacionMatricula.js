6028f03ba99c1b9c1708d0fc99f31ee3d61fdf7a
JuanDGM <juan84_10@hotmail.com>
2019-02-26 11:36:05 -0500

    cuarentaCinco

---
+++ js/Matricula/navegacionMatricula.js
@@ -0,0 +1,77 @@
+
+function misServiciosMatricula(){
+    
+     /* Agregar estilos a funcion notificacionmisServicios() */
+    document.getElementById('marcoMisServiciosModuloMatricula').style.display='block';
+    document.getElementById('btnAccesoSubModuloMisServiciosMatriculaId').style.borderTop='2px solid #757575';
+    document.getElementById('btnAccesoSubModuloMisServiciosMatriculaId').style.borderLeft='2px solid #757575';
+    document.getElementById('btnAccesoSubModuloMisServiciosMatriculaId').style.borderBottom='0px solid';
+    document.getElementById('btnAccesoSubModuloMisServiciosMatriculaId').style.borderRight='0px solid';
+    
+     /* Quitar estilos a funcion misCotizacionesMatricula() */
+    document.getElementById('marcoMisCotizacionesModuloMatricula').style.display='none';
+    document.getElementById('btnAccesoSubModuloMisCotizacionesMatriculaId').style.borderTop='0px solid';
+    document.getElementById('btnAccesoSubModuloMisCotizacionesMatriculaId').style.borderLeft='0px solid';
+    document.getElementById('btnAccesoSubModuloMisCotizacionesMatriculaId').style.borderBottom='2px solid #757575';
+    document.getElementById('btnAccesoSubModuloMisCotizacionesMatriculaId').style.borderRight='2px solid #757575';
+    
+     /* Quitar estilos a funcion nuevoServicioMatricula() */
+    document.getElementById('marcoNuevoServicioModuloMatricula').style.display='none';
+    document.getElementById('btnAccesoSubModuloNuevoServicioMatriculaId').style.borderTop='0px solid';
+    document.getElementById('btnAccesoSubModuloNuevoServicioMatriculaId').style.borderLeft='0px solid';
+    document.getElementById('btnAccesoSubModuloNuevoServicioMatriculaId').style.borderBottom='2px solid #757575';
+    document.getElementById('btnAccesoSubModuloNuevoServicioMatriculaId').style.borderRight='2px solid #757575';
+    
+}
+
+
+function misCotizacionesMatricula(){
+    
+     /* Quitar estilos a funcion notificacionmisServicios() */
+    document.getElementById('marcoMisServiciosModuloMatricula').style.display='none';
+    document.getElementById('btnAccesoSubModuloMisServiciosMatriculaId').style.borderTop='0px solid';
+    document.getElementById('btnAccesoSubModuloMisServiciosMatriculaId').style.borderLeft='0px solid';
+    document.getElementById('btnAccesoSubModuloMisServiciosMatriculaId').style.borderBottom='2px solid #757575';
+    document.getElementById('btnAccesoSubModuloMisServiciosMatriculaId').style.borderRight='2px solid #757575';
+    
+    
+     /* Agregar estilos a funcion misCotizacionesMatricula() */
+    document.getElementById('marcoMisCotizacionesModuloMatricula').style.display='block';
+    document.getElementById('btnAccesoSubModuloMisCotizacionesMatriculaId').style.borderTop='2px solid #757575';
+    document.getElementById('btnAccesoSubModuloMisCotizacionesMatriculaId').style.borderLeft='2px solid #757575';
+    document.getElementById('btnAccesoSubModuloMisCotizacionesMatriculaId').style.borderBottom='0px solid';
+    document.getElementById('btnAccesoSubModuloMisCotizacionesMatriculaId').style.borderRight='0px solid';
+    
+     /* Quitar estilos a funcion nuevoServicioMatricula() */
+    document.getElementById('marcoNuevoServicioModuloMatricula').style.display='none';
+    document.getElementById('btnAccesoSubModuloNuevoServicioMatriculaId').style.borderTop='0px solid';
+    document.getElementById('btnAccesoSubModuloNuevoServicioMatriculaId').style.borderLeft='0px solid';
+    document.getElementById('btnAccesoSubModuloNuevoServicioMatriculaId').style.borderBottom='2px solid #757575';
+    document.getElementById('btnAccesoSubModuloNuevoServicioMatriculaId').style.borderRight='2px solid #757575';
+    
+}
+
+function nuevoServicioMatricula(){
+    
+     /* Quitar estilos a funcion notificacionmisServicios() */
+    document.getElementById('marcoMisServiciosModuloMatricula').style.display='none';
+    document.getElementById('btnAccesoSubModuloMisServiciosMatriculaId').style.borderTop='0px solid';
+    document.getElementById('btnAccesoSubModuloMisServiciosMatriculaId').style.borderLeft='0px solid';
+    document.getElementById('btnAccesoSubModuloMisServiciosMatriculaId').style.borderBottom='2px solid #757575';
+    document.getElementById('btnAccesoSubModuloMisServiciosMatriculaId').style.borderRight='2px solid #757575';
+    
+    /* Quitar estilos a funcion misCotizacionesMatricula() */
+    document.getElementById('marcoMisCotizacionesModuloMatricula').style.display='none';
+    document.getElementById('btnAccesoSubModuloMisCotizacionesMatriculaId').style.borderTop='0px solid';
+    document.getElementById('btnAccesoSubModuloMisCotizacionesMatriculaId').style.borderLeft='0px solid';
+    document.getElementById('btnAccesoSubModuloMisCotizacionesMatriculaId').style.borderBottom='2px solid #757575';
+    document.getElementById('btnAccesoSubModuloMisCotizacionesMatriculaId').style.borderRight='2px solid #757575';
+    
+     /* Agregar estilos a funcion nuevoServicioMatricula() */
+    document.getElementById('marcoNuevoServicioModuloMatricula').style.display='block';
+    document.getElementById('btnAccesoSubModuloNuevoServicioMatriculaId').style.borderTop='2px solid #757575';
+    document.getElementById('btnAccesoSubModuloNuevoServicioMatriculaId').style.borderLeft='2px solid #757575';
+    document.getElementById('btnAccesoSubModuloNuevoServicioMatriculaId').style.borderBottom='0px solid';
+    document.getElementById('btnAccesoSubModuloNuevoServicioMatriculaId').style.borderRight='0px solid';
+    
+}
