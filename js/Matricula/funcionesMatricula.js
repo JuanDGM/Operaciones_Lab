02c9b7c416824f968ec50f604e099a4209fd845f
JuanDGM <juan84_10@hotmail.com>
2019-03-02 18:21:42 -0500

    cuarentaOcho

---
+++ js/Matricula/funcionesMatricula.js
@@ -0,0 +1,19 @@
+
+
+function mostrarServicioSolicitado(i){
+    
+   var MarcoDetalleSolicitadoId = "detalleServicioSolicitadoAprobado"+i;
+   var MarcoDetalleSolicitado = "#detalleServicioSolicitadoAprobado"+i;
+   
+   var esVisible = $(MarcoDetalleSolicitado).is(":visible");
+    
+    if(esVisible){
+        
+       document.getElementById(MarcoDetalleSolicitadoId).style.display='none'; 
+        
+    }else{
+       document.getElementById(MarcoDetalleSolicitadoId).style.display='block'; 
+    }
+    
+}
+
